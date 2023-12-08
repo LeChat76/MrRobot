0a45e4d383a0c0e98a06a0e3eb3d024b22a3a451
LeChat76 <cedrik76@msn.com>
2023-12-08 18:48:54 +0100

    debug /vlan/

--- network/forms/vlan_form.py
+++ network/forms/vlan_form.py
@@ -2,6 +2,7 @@ from django import forms
 from ..models import Vlan
 
 class VlanForm(forms.ModelForm):
+    description = forms.CharField(widget=forms.Textarea(attrs={'rows': 3, 'maxlength': 255}))
     class Meta:
         model = Vlan
         fields = ['id', 'name', 'description']
